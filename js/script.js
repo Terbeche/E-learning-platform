66bc9869509671864569d093b95352cff118cc59
Terbeche Mustapha <termus96@gmail.com>
2022-05-16 23:34:06 +0100

    add mobile menu and link to about page

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function showTheMenu() {
+  if (window.screen.width >= 768) {
+    return;
+  }
+  document.getElementById('overlay').style.display = 'flex';
+}
+
+function hideTheMenu() {
+  document.getElementById('overlay').style.display = 'none';
+}
+
+const navElement = document.getElementById('navigation');
+navElement.addEventListener('click', showTheMenu);
+
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', hideTheMenu);
+
+const menuLinks = document.getElementsByClassName('menu-link');
+for (let i = 0; i < menuLinks.length; i += 1) {
+  menuLinks[i].addEventListener('click', hideTheMenu);
+}
